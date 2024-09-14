c5d60ddb2881b4d368e4a45ac7e35a6ab9675a1b
Juan <juandavidgarcia947@gmail.com>
2024-09-13 21:34:04 -0500

    Number_race

---
+++ script_python/number_race/main.py
@@ -0,0 +1,12 @@
+'''
+Script description: Number Race
+Dev: Juan David G.
+Date: 13-09-2024
+'''
+from random import randint
+
+dice1 = randint(1,6)
+dice2 = randint(1,6)
+
+print(f"dice1: {dice1} ")
+print(f"dice2: {dice2} ")
